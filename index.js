178bcc16f4507027a9f1a6c65b11da2067447342
joshhoneypot <joshuaangugo@gmail.com>
2023-04-05 16:49:32 +0300

    search button attempt

--- index.js
+++ index.js
@@ -3,24 +3,57 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     fetchQuotes();
+    
 
 });
 
 const url = "https://api.quotable.io/random"
 
 
-//a function to fetch the data from the quotes API...fetchQuotes()
 
+
+//a function to fetch the data from the quotes API...fetchQuotes()
 function fetchQuotes(){
     fetch(url)
     .then(res => res.json())
     .then(quotes => {
-        document.getElementById("quote").innerHTML = quotes.content;
-        document.getElementById("author").innerHTML = quotes.author
+        const quote = document.getElementById("quote").innerHTML = quotes.content;
+        const author = document.getElementById("author").innerHTML = quotes.author
+        quote;
+        author;
     })
 }
 
-fetchQuotes(url);
+function copyText(htmlElement){
+    if (!htmlElement){
+        return;
+    }
+
+    let elementText = htmlElement.innerText;
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('value', elementText);
+    document.body.appendChild(inputElement);
+
+    inputElement.select();
+    inputElement.parentNode.removeChild();
+
+    document.execCommand('copy');
+     
+
+
+
+
+}
+document.querySelector("#copy-text").onclick = 
+function()
+{
+    copyText(document.querySelector("#quote"));
+
+}
+
+
+ 
+
 
 //a function to display random quote and aurthor on screen load...firstQuote()
 //figureout the functionality of the like button hen clicked and record the number of likes
