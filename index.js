da7648afc659e324ee8691b75723a5ffd4e1e438
joshhoneypot <joshuaangugo@gmail.com>
2023-04-05 11:38:49 +0300

    Fetch test 2

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //DOM content load
-    //Function calls
+
+const url = "https://api.quotable.io/random"
 
 //a function to fetch the data from the quotes API...fetchQuotes()
+
+function fetchQuotes(){
+    fetch(url)
+    .then(res => res.json())
+    .then(quote => {
+        console.log(quote);
+    })
+}
+
+fetchQuotes(url);
+
 //a function to display random quote and aurthor on screen load...firstQuote()
 //figureout the functionality of the like button hen clicked and record the number of likes
 //link the share button to an external socialmedia platform
