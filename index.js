4bda4e93ba91104d3106ff04ae32845359ee39bc
joshhoneypot <joshuaangugo@gmail.com>
2023-04-06 13:11:28 +0300

    Javascript comments

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 //DOM content load
 
 document.addEventListener("DOMContentLoaded", () => {
-   
     fetchQuotes();
 });
 
+//initializing variables
 const url = "https://api.quotable.io/random"
 const likeButton = document.querySelector(".like-button");
 const likeIcon = document.querySelector("#thumb-icon");
 const count = document.querySelector("#count");
 
+//line 13 to 25 updates the like button
 let clicked=false;
 
     likeButton.addEventListener("click", ()=>{
@@ -21,13 +22,10 @@ let clicked=false;
         clicked = false; 
         count.textContent--;
     }
-
 })
 
-
-//a function to fetch the data from the quotes API...fetchQuotes()
+//the following functions sends a fetch request to the remote api and converts the response to a .json file
 function fetchQuotes(){
-
     fetch(url)
     .then(res => res.json())
     .then(quotes => {
@@ -35,23 +33,37 @@ function fetchQuotes(){
         const author = document.getElementById("author").innerHTML = quotes.author
         quote;
         author;
-        
     })
 }
 
+//the following function is invoked on the click of the coppy button
+//it takes a htmlElement as a parameter 
 function copyText(htmlElement){
     if (!htmlElement){
         return;
     }
+
+    //initialises the variable of the text to be copied and also its content
     let elementText = htmlElement.innerText;
     let inputElement = document.createElement('input');
+
+    //gets the value of the created element
     inputElement.setAttribute('value', elementText);
+
+    //appends the input element with the targeted text  to the DOM but it does not display it on the page
     document.body.appendChild(inputElement);
+
+    //highlights the text in the inputelent
     inputElement.select()
+
+    //the following line copies the highlighted text to the clipboard
     document.execCommand('copy');
+
+    //deletes the created element
     document.body.removeChild(inputElement)
 }
 document.querySelector("#copy-text").onclick = 
+//the function below calls the copyText() function on click of the button
 function()
 {
     copyText(document.querySelector("#quote"));
